23d53b7c6b091d6498ad570a13214bdc69e2ca7b
Quantum-Revolution <2324015920@qq.com>
2020-06-27 00:18:58 +0800

    add on 2020/6/27

---
+++ 大作业/js/operater.js
@@ -0,0 +1,15 @@
+
+function login(){
+	window.location.href = "index.html";
+}
+
+function register(){
+	alert("此功能暂未开放。");
+}
+
+function sub(){
+	alert("感谢您的宝贵意见！");
+}
+function start(){
+	window.location.href = "About.html";
+}
